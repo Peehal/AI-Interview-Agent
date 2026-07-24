db8a69191c9ebcbc7a5e699e12e00e33f12fd516
“Peehal” <sinhapeehal@gmail.com>
2026-07-24 18:58:30 +0530

    Fix auth cookie for cross-origin production deploys

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -13,10 +13,11 @@ export const googleAuth = async (req,res) => {
             })
         }
         let token = await genToken(user._id)
+        const isProd = process.env.NODE_ENV === "production";
         res.cookie("token" , token , {
-            http:true,
-            secure:false,
-            sameSite:"strict",
+            httpOnly:true,
+            secure:isProd,
+            sameSite:isProd ? "none" : "lax",
             maxAge:7 * 24 * 60 * 60 * 1000
         })
 
